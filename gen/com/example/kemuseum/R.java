557e4f73411755c9a69d4da599e6aa320f2b54d7
abdullahizzuddiin@gmail.com <abdullahizzuddiin@gmail.com>
2014-04-04 14:42:27 +0700

    Penyempurnaan perpindahan pertab

--- gen/com/example/kemuseum/R.java
+++ gen/com/example/kemuseum/R.java
@@ -24,7 +24,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
+        public static final int action_settings=0x7f08000f;
         public static final int button_kataKunci=0x7f08000c;
         public static final int llAbout=0x7f080006;
         public static final int llBody=0x7f080007;
@@ -35,6 +35,7 @@ public final class R {
         public static final int llPencarian1=0x7f080009;
         public static final int llPencarian2=0x7f08000d;
         public static final int llPilMuseum=0x7f080003;
+        public static final int llViewCapaian=0x7f08000e;
         public static final int rlViewPencarian=0x7f080008;
         public static final int spinner_museum=0x7f08000a;
         public static final int text_kataKunci=0x7f08000b;
