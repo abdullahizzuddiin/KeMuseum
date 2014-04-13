6e00179791077ad11901f63e22288787b54ee89d
William Gozali <will.gozali@gmail.com>
2014-04-13 22:24:23 +0700

    sudah bisa muncul list pertanyaan

--- src/com/example/kemuseum/controller/ControllerPertanyaan.java
+++ src/com/example/kemuseum/controller/ControllerPertanyaan.java
@@ -2,12 +2,6 @@ package com.example.kemuseum.controller;
 
 import java.util.List;
 
-import android.util.Log;
-
-<<<<<<< HEAD
-=======
-import com.example.kemuseum.model.Barang;
->>>>>>> 665ffad7529ea91459db3463954a2f9774abd25f
 import com.example.kemuseum.model.Pertanyaan;
 import com.example.kemuseum.utils.MuseumManager;
 
