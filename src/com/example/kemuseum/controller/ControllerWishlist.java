085b041350eb77d875b446d71136bc0aa05d02d8
William Gozali <will.gozali@gmail.com>
2014-05-17 18:55:19 +0700

    betulin conflict yg tertinggal

--- src/com/example/kemuseum/controller/ControllerWishlist.java
+++ src/com/example/kemuseum/controller/ControllerWishlist.java
@@ -2,12 +2,9 @@ package com.example.kemuseum.controller;
 
 import java.util.List;
 
-<<<<<<< HEAD
-=======
 import org.json.JSONObject;
 
 import com.example.kemuseum.model.Capaian;
->>>>>>> 5226ee5789ff345bd0c94263da1c67b076bca188
 import com.example.kemuseum.model.Wishlist;
 import com.example.kemuseum.utils.JSONParser;
 import com.example.kemuseum.utils.WishlistManager;
@@ -29,12 +26,11 @@ public class ControllerWishlist {
 		wishlistManager.tambahWishlist(w);
 	}
 	
-<<<<<<< HEAD
 	public TextCaptcha getCaptcha(){
 		return new TextCaptcha(500, 200, 5, TextOptions.UPPERCASE_ONLY); 
-=======
+	}
+	
 	public String ambilJSON (Wishlist w) {
 		return JSONParser.toJSON(w);
->>>>>>> 5226ee5789ff345bd0c94263da1c67b076bca188
 	}
 }
